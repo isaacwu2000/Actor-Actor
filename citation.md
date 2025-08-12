c55605b7d00f5b657c262bac79993649974ee6a2
Isaac Wu <isaacwu2000@gmail.com>
2025-08-12 13:26:45 +0000

    prevented duplicates and irrelevant links from being extracted from the wikipedia page

--- citation.md
+++ citation.md
@@ -1,8 +1,11 @@
 # Citation
 ## Sources used
-* https://www.geeksforgeeks.org/python/beautifulsoup-scraping-link-from-html/
+* https://www.geeksforgeeks.org/, various sources
+* https://www.w3schools.com/python, various sources
 * https://www.crummy.com/software/BeautifulSoup/bs4/doc/
 * https://pypi.org/project/beautifulsoup4/
+* https://www.dataquest.io/cheat-sheet/regular-expressions-cheat-sheet/
+* AI search overview
 
 ## Signature
 Isaac Wu
--- get_page_links.py
+++ get_page_links.py
@@ -2,14 +2,22 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-def get_page_links(url):
-    links = []
+def get_wikipedia_page_links(url):
+    irrelevant_starts = ['Main_Page','Wikipedia','Help','Special','Talk','File','ISBN']
+    relevant_links = set()
+
     response = requests.get(url).text
     soup = BeautifulSoup(response, 'html.parser')
 
-    for link in soup.find_all('a', attrs={'href': re.compile("^https://")}):
-        links.append(link.get('href'))
-    
-    return links
+    for raw_link in soup.find_all('a', attrs={'href': re.compile("^/wiki/")}):
+        link = raw_link.get('href')
+        link_is_relevant = True
+        for start in irrelevant_starts:
+            if link.replace("/wiki/","").startswith(start):
+                link_is_relevant = False
+        if link_is_relevant:
+            relevant_links.add(link)
 
-print(get_page_links("https://www.geeksforgeeks.org/courses"))
+    return relevant_links
+
+print(get_wikipedia_page_links("https://en.wikipedia.org/wiki/Will_Smith"))
